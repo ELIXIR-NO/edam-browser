fb46811605f51bed318d9dc43ed4e95e34360866
Joshua Baskaran <joshbaskaran@gmail.com>
2025-05-19 13:14:45 +0200

    feat: add and enable react compiler
    Enabled the react compiler for automatic optimizations of react code.
    
    Greatly helps with performance, especially as the components grow in
    complexity.

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,25 @@ import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 
+const ReactCompilerConfig = {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  }
+	plugins: [
+		TanStackRouterVite({ autoCodeSplitting: true }),
+		viteReact({
+			babel: {
+				plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+			},
+		}),
+	],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
 });
