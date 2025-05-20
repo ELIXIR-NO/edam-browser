5e3b9c6ba893b06dd9757f2a36f4be66ea872f7f
Joshua Baskaran <joshbaskaran@gmail.com>
2025-05-20 09:59:04 +0200

    fix: add tailwindcss to vite config
    This was previously missed during the inital setup of shadcn/ui

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "node:path";
 
 const ReactCompilerConfig = {};
@@ -14,6 +15,7 @@ export default defineConfig({
 				plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
 			},
 		}),
+		tailwindcss(),
 	],
 	test: {
 		globals: true,
