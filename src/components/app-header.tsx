55a3b5f684f145c204fc9fbeaa40acb56086e9c1
Joshua Baskaran <joshbaskaran@gmail.com>
2025-05-20 15:28:42 +0200

    feat: update header with links

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,11 +1,38 @@
 import edamLogo from "/edam-logo.png";
 import { ThemeToggle } from "./theme-toggle";
+import { Link } from "lucide-react";
+import GitHub from "@/assets/svg/github";
 
 export function AppHeader() {
 	return (
-		<nav className="flex flex-row justify-between">
-			<img src={edamLogo} />
-			<ThemeToggle />
+		<nav className="flex flex-row gap-4 p-4">
+			<img src={edamLogo} width="100" />
+			<div className="flex flex-col gap-2">
+				<h1 className="text-3xl">EDAM Ontology</h1>
+				<p>
+					EDAM is a simple ontology of well established, familiar concepts that
+					are prevalent within bioinformatics.
+				</p>
+				<ul className="flex flex-row">
+					<li className="flex flex-row items-center gap-2">
+						<Link size="1rem" />
+						<a
+							href="https://github.com/edamontology/edamontology"
+							className="text-blue-600 visited:text-purple-900 hover:underline"
+						>
+							edamontology.org
+						</a>
+						<li className="flex flex-row items-center gap-2">
+							<GitHub fontSize="1rem" />
+							<a
+								href="https://github.com/ELIXIR-NO/edam-browser.git"
+								className="text-blue-600 visited:text-purple-900 hover:underline"
+							>
+								edam-browser
+							</a>
+						</li>
+					</li>
+				</ul>
 		</nav>
 	);
 }
